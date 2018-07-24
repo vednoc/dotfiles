3215b11250f8e01a5e4ad6e61e59dfbcbb4fc317
vednoc <vednoc@gmail.com>
2018-07-24 23:56:59 +0200

    added compton

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -5,6 +5,7 @@ xrdb -merge $HOME/.Xresources
 sxhkd &
 wal -R &
 $HOME/.config/polybar/launch.sh &
+$HOME/.config/compton/compton.conf &
 xautolock --time 20 --locker slock &
 redshift-gtk &
 nm-applet &
