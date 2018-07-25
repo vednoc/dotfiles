4cfc102ff0d2e2471ae927888a9b08a110d407e7
vednoc <vednoc@gmail.com>
2018-07-25 23:54:09 +0200

    fixed autostart script

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -5,7 +5,7 @@ xrdb -merge $HOME/.Xresources
 sxhkd &
 wal -R &
 $HOME/.config/polybar/launch.sh &
-$HOME/.config/compton/compton.conf &
+compton -f --config $HOME/.config/compton/compton.conf &
 xautolock --time 20 --locker slock &
 redshift-gtk &
 nm-applet &
