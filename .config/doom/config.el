8fa5991fe619f857ce89d797deec63bcae9e97fa
vednoc <vednoc@protonmail.com>
2019-03-15 20:47:46 +0100

    doom: enable git-gutter-mode

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -2,6 +2,9 @@
 
 ;; Place your private configuration here
 
+(global-git-gutter-mode t)
+
 (setq display-line-numbers-type nil
+      git-gutter:update-interval 1
       doom-font (font-spec :family "Iosevka" :size 14)
       doom-big-font (font-spec :family "Iosevka" :size 20))
