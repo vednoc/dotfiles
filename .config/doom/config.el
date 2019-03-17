ef17016f841740a3e2330cd9292943388196d9ad
vednoc <vednoc@protonmail.com>
2019-03-17 15:04:38 +0100

    doom: add sections and comments

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -6,17 +6,28 @@
 (global-git-gutter-mode t)
 
 (setq display-line-numbers-type nil
+
+      ;; show git changes every 1s
       git-gutter:update-interval 1
+
+      ;; org-mode configuration
       org-log-done 'time
       org-ellipsis "..."
       org-bullets-bullet-list '("⚫" "⚪")
       org-directory "~/doc/org"
+
+      ;; customize modeline
       doom-modeline-bar-width 1
       doom-modeline-major-mode-icon t
       doom-modeline-buffer-file-name-style 'truncate-upto-root
+
+      ;; set emacs theme
       doom-theme 'doom-sourcerer
+
+      ;; use my font of choice
       doom-font (font-spec :family "Iosevka" :size 14)
       doom-big-font (font-spec :family "Iosevka" :size 20))
 
+;; un/comment using alt+/ keybind
 (global-unset-key (kbd "M-/"))
 (map! :nei #'evilnc-comment-or-uncomment-lines)
